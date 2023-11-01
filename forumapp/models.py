46c199c16d2aab8465111b2691300dc46aa1343a
DgFn <nkita223@mail.ru>
2023-11-01 22:28:27 +0300

    Добавьте файлы проекта.

---
+++ forumapp/models.py
@@ -0,0 +1,15 @@
+﻿from django.db import models as m
+
+#class All_Section(m.Model):
+    
+
+class Section(m.Model):
+    name_section = m.CharField(max_length= 255,)#Вопрос чтобы всегда заполнялась
+    message = m.CharField(max_length=10000)
+    comment = m.CharField(max_length=5000)
+    #message_element = m.ForeignKey(All_Section, on_delete=m.SET_DEFAULT, default=None, null=True, blank=True)
+
+
+
+    
+# Create your models here.
---
+++ forumapp/views.py
@@ -0,0 +1,27 @@
+﻿from django.shortcuts import render
+from .models import Section
+from django.http import HttpResponseRedirect 
+
+def forumappView(request):
+    return render(request, 'todolist.html')
+
+def forumappView(request):
+    all_forum_items = Section.objects.all()
+    return render(request, 'todolist.html',
+    {'all_items':all_forum_items})
+
+def addforumView(request):
+    if request.method == "POST":
+        name_section = request.POST.get("name_section")
+        message = request.POST.get("message")
+        comment = request.POST.get("comment")
+        new_item = Section(name_section = name_section,message = message, comment = comment)
+        new_item.save()
+    return HttpResponseRedirect('/forumapp/') 
+
+def deleteForumView(request, i):
+    y = Section.objects.get(id= i)
+    y.delete()
+    return HttpResponseRedirect('/forumapp/') 
+
+
